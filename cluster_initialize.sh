d00a33802b07df2faaa4a2d95bf03175c0293733
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-24 19:13:44 -0400

    Update cluster_initialize.sh

--- cluster_initialize.sh
+++ cluster_initialize.sh
@@ -2,5 +2,5 @@ echo "Current Project $GCP_PROJECT"
 gsutil cp -r gs://"$GCP_PROJECT"/app/SparkTest-0.0.0/ SparkTest/
 cd SparkTest
 pip install -r requirements.txt .
-chmod u+x cloud_sql_proxy
-cloud_sql_proxy
+chmod u+x cloud_sql_proxy.sh
+cloud_sql_proxy.sh
