7abecdd6eea1f1843612ba863eaabaaca8edcd76
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-25 18:59:57 -0400

    Update gcp_onbaord.sh

--- gcp_onbaord.sh
+++ gcp_onbaord.sh
@@ -6,10 +6,10 @@ git clone https://github.com/arindampal2020/arindampal2020.git
 gsutil cp arindampal2020/cluster_initialize.sh gs://$PROJECT_ID/cluster
 echo "Downloading Application Project"
 git clone https://github.com/arindampal2020/SparkTest-0.0.0.git
-gsutil cp -r SparkTest-0.0.0 gs://$PROJECT_ID/app/SparkTest-0.0.0
+gsutil -m cp -r SparkTest-0.0.0 gs://$PROJECT_ID/app/SparkTest-0.0.0
 echo "Downloading Data"
 git clone https://github.com/dgadiraju/nyse_all.git
-gsutil cp -r nyse_all gs://$PROJECT_ID/data
+gsutil -m cp -r nyse_all gs://$PROJECT_ID/data
 echo "Creating MY SQL instances in Cloud..."
 gcloud sql instances create myappdb \
 --database-version=MYSQL_5_7  \
