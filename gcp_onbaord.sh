11079f30b8837ace57eddf1295483e59c2500cc4
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-25 14:22:45 -0400

    Update gcp_onbaord.sh

--- gcp_onbaord.sh
+++ gcp_onbaord.sh
@@ -1,4 +1,6 @@
-git clone arindampal2020/SparkTest-0.0.0
-gsutil cp -r SparkTest-0.0.0 gs://"$GCP_PROJECT"/app/SparkTest-0.0.0
+export PROJECT_ID=$(gcloud config get-value project)
+echo $PROJECT_ID
+git clone https://github.com/arindampal2020/SparkTest-0.0.0.git
+gsutil cp -r SparkTest-0.0.0 gs://$PROJECT_ID/app/SparkTest-0.0.0
 git clone https://github.com/dgadiraju/nyse_all.git
-gsutil cp -r nyse_all gs://"$GCP_PROJECT"/data
+gsutil cp -r nyse_all gs://$PROJECT_ID/data
