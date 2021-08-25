74a0301a6ab32d55d8800e64f91b244964bc43d4
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-25 17:06:17 -0400

    Update cluster_initialize.sh

--- cluster_initialize.sh
+++ cluster_initialize.sh
@@ -3,6 +3,3 @@ echo $PROJECT_ID
 gsutil -m cp -r gs://$PROJECT_ID/app/SparkTest-0.0.0/ .
 cd SparkTest-0.0.0
 pip install -r requirements.txt .
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
-chmod +x cloud_sql_proxy
-cloud_sql_proxy
