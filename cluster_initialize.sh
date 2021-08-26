c29193f70fec89dd83b0b516874ed4e7f04361aa
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 19:31:54 -0400

    t-talks-handson

--- cluster_initialize.sh
+++ cluster_initialize.sh
@@ -1,5 +1,5 @@
 export PROJECT_ID=$(gcloud config get-value project)-ml
 echo "Current Project- $PROJECT_ID "
-gsutil -m cp -r gs://$PROJECT_ID/app/SparkTest-0.0.0/ .
-cd SparkTest-0.0.0
+gsutil -m cp -r gs://$PROJECT_ID/app/t-talks-handson/ .
+cd t-talks-handson
 pip install -r requirements.txt .
