12f8ef048e971d7d3c424e35dd586e208307113e
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 18:16:26 -0400

    Create hands-on-cluster.sh

---
+++ hands-on-cluster.sh
@@ -0,0 +1,10 @@
+export PROJECT_ID=$(gcloud config get-value project)-ml
+export REGION="us-central1"
+echo "Current Project id $PROJECT_ID and region is $REGION"
+export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
+echo "Updating MY SQL network"
+gcloud sql instances patch flights --authorized-networks $ADDRESS
+echo "Updating config.xml"
+gsutil cp ./t-talks-handson/common/config.xml gs://$PROJECT_ID/app/t-talks-handson/common/config.xml
+echo "Creating Pyspark Cluster..."
+gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=3 --max-age="2h"
