35be0ddda81df6d94bc2f6823de573077c02c5f4
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 12:19:45 -0400

    Update mysql_insert_stock.sh

--- mysql_insert_stock.sh
+++ mysql_insert_stock.sh
@@ -2,10 +2,11 @@
 export PROJECT_ID=$(gcloud config get-value project)
 export REGION="us-central1"
 export MYSQLIP=$(gcloud sql instances describe myappdb --format="value(ipAddresses.ipAddress)")
+mysql -uroot -pchangeme -h $MYSQLIP < 01_nyse_create_table_quickstart.sql
 gunzip nyse_all/nyse_data/NYSE*.gz
 for f in 'ls nyse_all/nyse_data/NYSE*txt'
 do
 mv f stock_eod_${counter}
-mysqlimport --local --host=$MYSQLIP --user=root --pchangeme --fields-terminated-by=','  --lines-terminated-by='\n' nyse stock_eod_*
+mysqlimport --local --host=$MYSQLIP --user=root -pchangeme --fields-terminated-by=','  --lines-terminated-by='\n' nyse stock_eod_*
 counter=$((counter+1))
 done
