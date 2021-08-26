81feb12abfe521731d78e1a504d14375b66cd617
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 15:05:44 -0400

    Update mysql_insert_stock.sh

--- mysql_insert_stock.sh
+++ mysql_insert_stock.sh
@@ -1,12 +1,24 @@
 
 export PROJECT_ID=$(gcloud config get-value project)
 export REGION="us-central1"
-export MYSQLIP=$(gcloud sql instances describe myappdb --format="value(ipAddresses.ipAddress)")
+export MYSQLIP=$(gcloud sql instances describe flights --format="value(ipAddresses.ipAddress)")
 mysql -uroot -pchangeme -h $MYSQLIP < 01_nyse_create_table_quickstart.sql
-gunzip nyse_all/nyse_data/NYSE*.gz
-for f in 'ls nyse_all/nyse_data/NYSE*txt'
+gunzip ./nyse_all/nyse_data/
+for f in ./nyse_all/nyse_data/*.txt
 do
-mv f stock_eod_${counter}
-mysqlimport --local --host=$MYSQLIP --user=root -pchangeme --fields-terminated-by=','  --lines-terminated-by='\n' nyse stock_eod_*
-counter=$((counter+1))
+  unlink stock_eod.txt
+  ln -s $f stock_eod.txt
+  mysqlimport \
+    --host=$MYSQLIP \
+    --user=root \
+    --password=Passw0rd \
+    --fields-terminated-by=',' \
+    --lines-terminated-by='\n' \
+    --local \
+    --lock-tables \
+    --verbose \
+    nyse stock_eod.txt
+      
+echo "Done: '"$f"' at $(date)"
 done
+
