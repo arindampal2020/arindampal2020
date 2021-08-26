dae5728cfe240f4f2804f24e8abb79d6d2ba78fc
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 11:36:51 -0400

    Update DataProc_Submit_Stage_Insert_job.sh

--- DataProc_Submit_Stage_Insert_job.sh
+++ DataProc_Submit_Stage_Insert_job.sh
@@ -4,4 +4,4 @@ export PY_SCRIPT="gs://$PROJECT_ID/app/SparkTest-0.0.0/PythonPrep/stock_insert.p
 export JAR="gs://$PROJECT_ID/app/SparkTest-0.0.0/common/jar/mysql-connector-java-8.0.26.jar"
 export DATA="gs://$PROJECT_ID/data/nyse_data"
 echo "Submitting Pyspark Jobs \n Script- $PY_SCRIPT \n Jar- $JAR "
-gcloud dataproc jobs submit pyspark --cluster=cluster-1417 "$PY_SCRIPT" --jars=$JAR  --region $REGION -- "nyse" $DATA
+gcloud dataproc jobs submit pyspark --cluster=my_cluster "$PY_SCRIPT" --jars=$JAR  --region $REGION -- "nyse" $DATA
