bcce3d26a313eb4a35e10b15f7d71a4ba2775249
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 14:29:11 -0400

    Update gcp_onbaord.sh

--- gcp_onbaord.sh
+++ gcp_onbaord.sh
@@ -13,7 +13,3 @@ gsutil -m cp -r nyse_all gs://$PROJECT_ID/data
 echo "Creating MY SQL instances in Cloud..."
 #gcloud sql instances create myappdb --database-version=MYSQL_5_7  --cpu=2 --memory=7680MB --region=$REGION --storage-type SSD --storage-size 10GB --activation-policy=ALWAYS
 #gcloud sql users set-password root --host % --instance myappdb  --password changeme
-export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
-gcloud sql instances patch flights --authorized-networks $ADDRESS
-echo "Creating Pyspark Cluster..."
-gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=5 --max-age="2h"
