18557060adac73944bf885c2de8824946faa2897
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 18:15:01 -0400

    Create on_board_hands_on.sh

---
+++ on_board_hands_on.sh
@@ -0,0 +1,15 @@
+export PROJECT_ID=$(gcloud config get-value project)-ml
+export REGION="us-central1"
+echo "Current Project id $PROJECT_ID and region is $REGION"
+echo "Downloading onboarding  scripts"
+git clone https://github.com/arindampal2020/arindampal2020.git
+gsutil cp arindampal2020/cluster_initialize.sh gs://$PROJECT_ID/cluster_initialize.sh
+echo "Downloading Application Project"
+git clone https://github.com/arindampal2020/t-talks-handson.git
+gsutil -m cp -r t-talks-handson gs://$PROJECT_ID/app/t-talks-handson
+echo "Downloading Data"
+git clone https://github.com/dgadiraju/nyse_all.git
+gsutil -m cp -r nyse_all gs://$PROJECT_ID/data
+echo "Creating MY SQL instances in Cloud..."
+#gcloud sql instances create myappdb --database-version=MYSQL_5_7  --cpu=2 --memory=7680MB --region=$REGION --storage-type SSD --storage-size 10GB --activation-policy=ALWAYS
+#gcloud sql users set-password root --host % --instance myappdb  --password changeme
