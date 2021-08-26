394f6d63a8825f6d605d9d7e26d4d353c12fdef0
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 14:24:27 -0400

    Update gcp_onbaord.sh

--- gcp_onbaord.sh
+++ gcp_onbaord.sh
@@ -3,7 +3,7 @@ export REGION="us-central1"
 echo "Current Project id $PROJECT_ID and region is $REGION"
 echo "Downloading onboarding  scripts"
 git clone https://github.com/arindampal2020/arindampal2020.git
-gsutil cp arindampal2020/cluster_initialize.sh gs://$PROJECT_ID/cluster
+gsutil cp arindampal2020/cluster_initialize.sh gs://$PROJECT_ID/cluster_initialize.sh
 echo "Downloading Application Project"
 git clone https://github.com/arindampal2020/SparkTest-0.0.0.git
 gsutil -m cp -r SparkTest-0.0.0 gs://$PROJECT_ID/app/SparkTest-0.0.0
@@ -11,9 +11,9 @@ echo "Downloading Data"
 git clone https://github.com/dgadiraju/nyse_all.git
 gsutil -m cp -r nyse_all gs://$PROJECT_ID/data
 echo "Creating MY SQL instances in Cloud..."
-gcloud sql instances create myappdb --database-version=MYSQL_5_7  --cpu=2 --memory=7680MB --region=$REGION --storage-type SSD --storage-size 10GB --activation-policy=ALWAYS
-gcloud sql users set-password root --host % --instance myappdb  --password changeme
+#gcloud sql instances create myappdb --database-version=MYSQL_5_7  --cpu=2 --memory=7680MB --region=$REGION --storage-type SSD --storage-size 10GB --activation-policy=ALWAYS
+#gcloud sql users set-password root --host % --instance myappdb  --password changeme
 export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
-gcloud sql instances patch myappdb --authorized-networks $ADDRESS
+gcloud sql instances patch flights --authorized-networks $ADDRESS
 echo "Creating Pyspark Cluster..."
 gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=5 --max-age="2h"
