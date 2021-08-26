acdf30f73ca69141ad26fa8ccc0b606f9c595d43
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 14:07:41 -0400

    Update cluster_initialize.sh

--- cluster_initialize.sh
+++ cluster_initialize.sh
@@ -1,7 +1,5 @@
-export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_ID=$(gcloud config get-value project)-ml
 echo "Current Project- $PROJECT_ID "
 gsutil -m cp -r gs://$PROJECT_ID/app/SparkTest-0.0.0/ .
 cd SparkTest-0.0.0
 pip install -r requirements.txt .
-export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
-gcloud sql instances patch myappdb --authorized-networks $ADDRESS
