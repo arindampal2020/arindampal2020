4d6b1aa9860f20c5d524ad6891e8699540a64d1b
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 13:20:36 -0400

    Update gcp_onboard_flights.sh

--- gcp_onboard_flights.sh
+++ gcp_onboard_flights.sh
@@ -1,1 +1,24 @@
+git clone  https://github.com/GoogleCloudPlatform/data-science-on-gcp/
+cd data-science-on-gcp/03_sqlstudio
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export BUCKET=${PROJECT_ID}-ml
+echo "Create Cloud Instances"
+gcloud sql instances create flights   --tier=db-n1-standard-1 --activation-policy=ALWAYS
+gcloud sql users set-password root --host % --instance flights  --password Passw0rd
 
+export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
+gcloud sql instances patch flights --authorized-networks $ADDRESS
+
+MYSQLIP=$(gcloud sql instances describe flights --format="value(ipAddresses.ipAddress)")
+echo $MYSQLIP
+
+mysql --host=$MYSQLIP --user=root   -pPassw0rd --verbose < create_table.sql
+
+counter=0
+for FILE in 201501.csv 201502.csv; do
+   gsutil cp gs://$BUCKET/flights/raw/$FILE \
+             flights.csv-${counter}
+   counter=$((counter+1))
+done
+
+mysqlimport --local --host=$MYSQLIP --user=root -p Passw0rd --ignore-lines=1 --fields-terminated-by=',' bts flights.csv-*
