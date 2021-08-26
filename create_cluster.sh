6b2a7f472b4d3a8e9c43e8a1aad2e17098fa1139
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-26 16:11:21 -0400

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -7,4 +7,4 @@ gcloud sql instances patch flights --authorized-networks $ADDRESS
 echo "Updating config.xml"
 gsutil cp ./SparkTest-0.0.0/common/config.xml gs://$PROJECT_ID/app/SparkTest-0.0.0/common/config.xml
 echo "Creating Pyspark Cluster..."
-gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=5 --max-age="2h"
+gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=3 --max-age="2h"
