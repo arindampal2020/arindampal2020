df8d3de4c419ded9eb17ed009121d04bad551383
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-27 13:40:33 -0400

    Update DataProc_Submit_Stage_Connectivity_job.sh

--- DataProc_Submit_Stage_Connectivity_job.sh
+++ DataProc_Submit_Stage_Connectivity_job.sh
@@ -1,6 +1,6 @@
-export PROJECT_ID=$(gcloud config get-value project)-ml
+export BUCKET=$(gcloud config get-value project)-ml
 export REGION="us-central1"
-export PY_SCRIPT="gs://$PROJECT_ID/app/SparkTest-0.0.0/PythonPrep/stock_export.py"
-export JAR="gs://$PROJECT_ID/app/SparkTest-0.0.0/common/jar/mysql-connector-java-8.0.26.jar"
+export PY_SCRIPT="gs://$BUCKET/app/SparkTest-0.0.0/PythonPrep/stock_export.py"
+export JAR="gs://$BUCKET/app/SparkTest-0.0.0/common/jar/mysql-connector-java-8.0.26.jar"
 echo "Submitting Pyspark Jobs \n Script- $PY_SCRIPT \n Jar- $JAR "
-gcloud dataproc jobs submit pyspark --cluster=my-cluster "$PY_SCRIPT" --jars=$JAR --region $REGION -- "nyse" "stock_eod"
+gcloud dataproc jobs submit pyspark --cluster=my-cluster "$PY_SCRIPT" --jars=$JAR --region $REGION --zone ${REGION}-a -- "nyse" "stock_eod"
