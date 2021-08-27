c684525b097abed0d7ebbfd66de0d152cefe2cb4
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-27 17:30:11 -0400

    Update hands-on-cluster.sh

--- hands-on-cluster.sh
+++ hands-on-cluster.sh
@@ -8,8 +8,4 @@ gcloud sql instances patch flights --authorized-networks $ADDRESS
 echo "Updating config.xml"
 gsutil cp ./t-talks-handson/main/config.xml gs://$BUCKET/app/t-talks-handson/main/config.xml
 echo "Creating Pyspark Cluster..."
-gcloud dataproc clusters create my-cluster --region=$REGION --zone ${REGION}-a --initialization-actions="gs://${BUCKET}/cluster_initialize.sh" --num-workers=2 --max-age="2h" --driver-log-levels root=FATAL
-
-CLUSTERIP=$(gcloud sql instances describe my-cluster --format="value(ipAddresses.ipAddress)")
-echo $CLUSTERIP
-gcloud sql instances patch flights --authorized-networks $CLUSTERIP
+gcloud dataproc clusters create my-cluster --region=$REGION --zone ${REGION}-a --initialization-actions="gs://${BUCKET}/cluster_initialize.sh" --num-workers=2 --max-age="2h"
