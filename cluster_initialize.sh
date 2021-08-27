7beefb6baac418d1e9d56b3248930028d85dc0df
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-27 13:42:53 -0400

    Update cluster_initialize.sh

--- cluster_initialize.sh
+++ cluster_initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PROJECT_ID=$(gcloud config get-value project)-ml
-echo "Current Project- $PROJECT_ID "
-gsutil -m cp -r gs://$PROJECT_ID/app/t-talks-handson/ .
+export BUCKET=$(gcloud config get-value project)-ml
+echo "Current bucket- $BUCKET "
+gsutil -m cp -r gs://$BUCKET/app/t-talks-handson/ .
 cd t-talks-handson
 pip install -r requirements.txt .
