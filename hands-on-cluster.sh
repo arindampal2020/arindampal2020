f656f4a850f12b66f4b7b70ab33b4b72867efd18
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-27 13:35:26 -0400

    Update hands-on-cluster.sh

--- hands-on-cluster.sh
+++ hands-on-cluster.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-export PROJECT_ID=$(gcloud config get-value project)-ml
+export BUCKET=$(gcloud config get-value project)-ml
 export REGION="us-central1"
-echo "Current Project id $PROJECT_ID and region is $REGION"
+echo "Current Buket is $BUCKET and region is $REGION"
 export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
 echo "Updating MY SQL network"
 gcloud sql instances patch flights --authorized-networks $ADDRESS
 echo "Updating config.xml"
-gsutil cp ./t-talks-handson/main/config.xml gs://$PROJECT_ID/app/t-talks-handson/main/config.xml
+gsutil cp ./t-talks-handson/main/config.xml gs://$BUCKET/app/t-talks-handson/main/config.xml
 echo "Creating Pyspark Cluster..."
-gcloud dataproc clusters create my-cluster --region=$REGION --zone ${REGION}-a --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=2 --max-age="2h" --driver-log-levels root=FATAL
+gcloud dataproc clusters create my-cluster --region=$REGION --zone ${REGION}-a --initialization-actions="gs://${BUCKET}/cluster_initialize.sh" --num-workers=2 --max-age="2h" --driver-log-levels root=FATAL
 
 CLUSTERIP=$(gcloud sql instances describe my-cluster --format="value(ipAddresses.ipAddress)")
 echo $CLUSTERIP
