03035dd847351d336c7c94a95be3ac560d1e0912
arindampal2020 <74685468+arindampal2020@users.noreply.github.com>
2021-08-27 13:44:08 -0400

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,10 +1,10 @@
-export PROJECT_ID=$(gcloud config get-value project)-ml
+export BUCKET=$(gcloud config get-value project)-ml
 export REGION="us-central1"
-echo "Current Project id $PROJECT_ID and region is $REGION"
+echo "Current bucket id $BUCKET and region is $REGION"
 export ADDRESS=$(wget -qO - http://ipecho.net/plain)/32
 echo "Updating MY SQL network"
 gcloud sql instances patch flights --authorized-networks $ADDRESS
 echo "Updating config.xml"
-gsutil cp ./SparkTest-0.0.0/common/config.xml gs://$PROJECT_ID/app/SparkTest-0.0.0/common/config.xml
+gsutil cp ./SparkTest-0.0.0/common/config.xml gs://$BUCKET/app/SparkTest-0.0.0/common/config.xml
 echo "Creating Pyspark Cluster..."
-gcloud dataproc clusters create my-cluster --region=$REGION --initialization-actions="gs://${PROJECT_ID}/cluster_initialize.sh" --num-workers=3 --max-age="2h"
+gcloud dataproc clusters create my-cluster --region=$REGION --zone ${REGION}-a --initialization-actions="gs://${BUCKET}/cluster_initialize.sh" --num-workers=3 --max-age="2h"
